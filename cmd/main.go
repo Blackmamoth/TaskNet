782762704fddd411f666f48eda1e8d7f643f8bc2
blackmamoth <blackmamoth83@gmail.com>
2024-07-21 22:39:24 +0530

    changed a typo in cmd/main.go (invalid format argument)

--- cmd/main.go
+++ cmd/main.go
@@ -3,13 +3,12 @@ package main
 import (
 	"github.com/blackmamoth/tasknet/cmd/api"
 	"github.com/blackmamoth/tasknet/pkg/config"
-	"github.com/blackmamoth/tasknet/pkg/db"
 )
 
 func main() {
 	apiServer := api.NewAPIServer(config.GlobalConfig.AppConfig.APP_HOST, config.GlobalConfig.AppConfig.APP_PORT, nil)
 
 	if err := apiServer.Run(); err != nil {
-		config.Logger.CRITICAL("Application terminated: %v", db.Conn)
+		config.Logger.CRITICAL("Application terminated: %v", err)
 	}
 }
